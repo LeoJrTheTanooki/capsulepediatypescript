ed3d60db101c879ed1358990d14a23b56082c638
LeoJrTheTanooki <aceraccoon@proton.me>
2024-09-17 11:22:14 -0700

    Adjusted home page

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,34 @@ import NavbarComponent from "./components/NavbarComponent";
 import UnovaDexComponent from "./components/UnovaDexComponent";
 
 function App() {
+  const [queryHook, setQueryHook] = useState("");
 
-    const [queryHook, setQueryHook] = useState('')
-
-    useEffect(() => {
-      
-    console.log(queryHook)
-    }, [queryHook])
-    
+  useEffect(() => {
+    console.log(queryHook);
+  }, [queryHook]);
 
   return (
-    <>
+    <div className=" flex flex-col h-screen">
       <NavbarComponent query={queryHook} setQuery={setQueryHook} />
-      <UnovaDexComponent query={queryHook} setQuery={setQueryHook}/>
-    </>
+      <UnovaDexComponent query={queryHook} setQuery={setQueryHook} />
+      <div className="flex flex-wrap justify-between bg-white mt-auto">
+        <div className="flex w-full">
+          <p className="m-1">
+            API provided by{" "}
+            <a
+              className=" text-blue-700 font-semibold"
+              href="https://pokeapi.co/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              PokeAPI
+            </a>
+          </p>
+          <p className="m-1">Version 0.1</p>
+        </div>
+        <p className="bg-white m-1">Features to come: Better formatted text, List of Pokemon as landing page, Gen 5 search limit, Multiple generation styles, Favorites</p>
+      </div>
+    </div>
   );
 }
 
